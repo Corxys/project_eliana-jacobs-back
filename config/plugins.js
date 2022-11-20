1c2a13e96adb9ff6b5e700f64b3b25eb13efca0d
Corxys <jennblngr@gmail.com>
2022-11-20 15:50:38 +0100

    :wrench: graphql playground always turn to true

--- config/plugins.js
+++ config/plugins.js
@@ -17,7 +17,7 @@ module.exports = ({ env }) => ({
     config: {
       endpoint: '/graphql',
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
